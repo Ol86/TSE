95a08d21091dd7029228f88a4990c2bbee59eb43
ufzpa <ufzpa@student.kit.edu>
2023-11-29 16:49:09 +0100

    Change scripts

--- Backend/Server/server_backend/main.sh
+++ Backend/Server/server_backend/main.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
+# Basic script to delete and restart the docker with new changes.
+
+# Stop all container
 docker stop $(docker ps -q)
 
+# Delete all container
 docker rm $(docker ps -a -q)
 
+# Delete all Images
 docker rmi $(docker images -aq)
 
+# Delete all Volumes
 docker volume rm $(docker volume ls -q)
 
+# Create new Project
 docker compose --project-name tse up -d
 
+# Create Mirgrations of the backend
 docker compose -p tse exec server-backend python manage.py makemigrations base
 
+# Wait 5 sec before migrating
 sleep 5
 
+# Migrate for the database
 docker compose -p tse exec server-backend python manage.py migrate
 
+# Create a superuser for the server-backend
 docker compose -p tse exec server-backend python manage.py createsuperuser --noinput
 
+# Wait 5 sec
 sleep 5
 
+# Restart server-backend to submit all changes
 docker compose -p tse restart server-backend
