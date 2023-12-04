dbd7db465bc4ae6750ba661473636953328fd83d
ufzpa <ufzpa@student.kit.edu>
2023-12-04 21:45:28 +0100

    Add REST-API To Backend

--- Backend/Server/server_backend/server_backend/urls.py
+++ Backend/Server/server_backend/server_backend/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+from base import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+    path('api/', include(router.urls)),
+    path('api_auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
 ]
+
+urlpatterns += router.urls
