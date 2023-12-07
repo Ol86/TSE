c0c3bc9d2d9af7d06131419907c73ea64eaae6fa
ufzpa <ufzpa@student.kit.edu>
2023-12-07 10:52:04 +0100

    Implement the first kind of Models

--- Backend/Server/server_backend/base/models.py
+++ Backend/Server/server_backend/base/models.py
@@ -1,28 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Watches(models.Model):
-    """ This class generates the table of the watches inside the database.
-    
-    :param models.Model: This parameter specifies the generation progres of a new watch.
-    """
+class QuestionExperimentHandler(models.Model):
     id = models.AutoField(primary_key=True)
-    serialnumber = models.CharField(max_length=200)
+    experiment_id = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Questions, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['id']
 
     def __str__(self):
-        """ This function is the constructor.
-        
-        :return It returns the serialnumber of each watch.
-        """
-        return self.serialnumber
+        return self.id
 
 class Experiment(models.Model):
     """
     """
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
-    participant_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    watch_id = models.ForeignKey(Watches, on_delete=models.CASCADE)
+    Questions = models.ForeignKey(QuestionExperimentHandler, on_delete=models.CASCADE)
+    watch_id = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -30,3 +26,22 @@ class Experiment(models.Model):
 
     def __str__(self):
         return self.title
+
+class Questions(models.Model):
+    id = models.AutoField(primary_key=True)
+    question = models.CharField(max_length=200)
+    button1 = models.BooleanField(default=True)
+    button1_text = models.CharField(max_length=50)
+    button2 = models.BooleanField(default=True)
+    button2_text = models.CharField(max_length=50)
+    button3 = models.BooleanField(default=True)
+    button3_text = models.CharField(max_length=50, null=True)
+    button4 = models.BooleanField(default=True)
+    button4_text = models.CharField(max_length=50, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.question
