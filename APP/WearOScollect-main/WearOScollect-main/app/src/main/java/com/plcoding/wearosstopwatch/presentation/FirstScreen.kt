0f99efe96ec8d521fa6d1f9b1222d044197f5ca1
jurij <uqitn@student.kit.edu>
2023-12-13 22:27:13 +0100

    First Screen implemented, Go Back Button at Stopwatch Screen, Accept Button on purpose not merged with Play Button

---
+++ APP/WearOScollect-main/WearOScollect-main/app/src/main/java/com/plcoding/wearosstopwatch/presentation/FirstScreen.kt
@@ -0,0 +1,13 @@
+package com.plcoding.wearosstopwatch.presentation
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class FirstScreen(text: Unit) : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(View(this))
+    }
+}
