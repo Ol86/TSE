a3ce6c1f6084b0ff6a4a258a1c2ca2669aa7b707
alejandrovillagomez <alejandrovillago@gmail.com>
2024-01-10 16:38:16 +0100

    Sample Get chart

---
+++ ApacheSuperset/Automatization/main.py
@@ -0,0 +1,19 @@
+import requests
+
+# Replace with your Superset instance URL and credentials
+url = 'http://193.196.36.62:8088'
+username = 'admin'
+password = 'admin'
+
+# Authenticate
+auth_res = requests.post(f'{url}/security/login', json={
+  'username': username,
+  'password': password
+})
+auth_token = auth_res.json()['access_token']
+
+# Fetch dashboards
+headers = {'Authorization': f'Bearer {auth_token}'}
+dashboards_res = requests.get(f'{url}/api/v1/dashboard/', headers=headers)
+dashboards = dashboards_res.json()
+print(dashboards)
