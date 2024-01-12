86db829152b8fbd3a8782fbcf4ebe06a9ad804bd
David67 <davidreis@gmx.de>
2024-01-12 15:32:46 +0100

    method createDashboard() implemented

--- ApacheSuperset/Automatization/api.py
+++ ApacheSuperset/Automatization/api.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import test
 
 def saveToFile(result):
     with open("test.json", "w") as output:
@@ -16,4 +17,33 @@ def getCharts(url, auth_token):
     charts_res = requests.get(f'{url}/api/v1/chart/', headers = headers)
     charts = charts_res.json()
     saveToFile(json.dumps(charts, indent=4))
+
+def createDashboard(url, auth_token):
+
+    headers = {
+        'Authorization': f'Bearer {auth_token}',
+        'X-CSRFToken': test.csrf_token,
+    }
+
+    body = {
+        "certification_details": "string",
+        "certified_by": "string",
+        "css": "string",
+        "dashboard_title": "test2",
+        "external_url": "string",
+        "is_managed_externally": True,
+        "json_metadata": "",
+        "owners": [
+            1
+        ],
+        "position_json": "",
+        "published": True,
+        "roles": [
+            1
+        ],
+        "slug": "string2"
+    }
+
+    dashboards_res = test.session.post(f'{url}/api/v1/dashboard/', headers=headers, json=body)
+    print(dashboards_res.json())
 
--- ApacheSuperset/Automatization/test.py
+++ ApacheSuperset/Automatization/test.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import api
 
 session = requests.Session()
 
@@ -19,31 +20,9 @@ print(auth_token)
 csrf_token = session.get(f'{url}/api/v1/security/csrf_token/', headers={'Authorization': f'Bearer {auth_token}'}).json()['result']
 print(csrf_token)
 
-headers = {
-    'Authorization': f'Bearer {auth_token}',
-    'X-CSRFToken': csrf_token,
-}
+api.createDashboard(url, auth_token)
 
-body = {
-    "certification_details": "string",
-    "certified_by": "string",
-    "css": "string",
-    "dashboard_title": "test2",
-    "external_url": "string",
-    "is_managed_externally": True,
-    "json_metadata": "",
-    "owners": [
-        1
-    ],
-    "position_json": "",
-    "published": True,
-    "roles": [
-        1
-    ],
-    "slug": "string2"
-}
 
-dashboards_res = session.post(f'{url}/api/v1/dashboard/', headers=headers, json=body)
-print(dashboards_res.json())
+
 
 session.close()
