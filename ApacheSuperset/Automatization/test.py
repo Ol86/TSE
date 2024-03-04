e19897c74a0eb60f3c3bfa76d804aab7da557a1e
alejandrovillagomez <alejandrovillago@gmail.com>
2024-03-04 17:25:37 +0100

    userverwaltung für die Owners

--- ApacheSuperset/Automatization/test.py
+++ ApacheSuperset/Automatization/test.py
@@ -26,30 +26,47 @@ csrf_token = \
 
 print(csrf_token)
 
-#api.createDashboard(url, csrf_token, auth_token, session)
-#api.createChart(url, csrf_token, auth_token, session)
-#api.getCharts(url, auth_token)
-#new_chart = api.createChart(url, csrf_token, auth_token, session)
-#api.saveToFile(json.dumps(new_chart, indent=4))
+# api.createDashboard(url, csrf_token, auth_token, session)
+# api.createChart(url, csrf_token, auth_token, session)
+# api.getCharts(url, auth_token)
+# new_chart = api.createChart(url, csrf_token, auth_token, session)
+# api.saveToFile(json.dumps(new_chart, indent=4))
+
+# api.getDatasets(url, auth_token)
+# api.createDataset(url, csrf_token, auth_token, session)
+
+# ----------------------------------------------------------------
+
+maedche = {
+    "first_name": "user",
+    "id": 2,
+    "last_name": "Maedche"
+}
+
+ole = {
+    "first_name": "user",
+    "id": 3,
+    "last_name": "OleMenke"
+}
+
+
+#api.getDatasets(url, auth_token, session)
+#print(api.get_datasource_id(api.getDatasets(url, auth_token, session), owner.get('last_name') + "_base_session"))
+api.createStandarizedDashboard(url, csrf_token, auth_token, session, maedche)
+api.createStandarizedDashboard(url, csrf_token, auth_token, session, ole)
 
 #api.getDatasets(url, auth_token)
-#api.createDataset(url, csrf_token, auth_token, session)
+# api.getCharts(url, auth_token)
+# api.getDashboards(url, auth_token)
+# api.get_charts_from_dashboard(api.getCharts(url, auth_token), 3)
 
-#----------------------------------------------------------------
+# api.createChart_numberOfSessions(url, csrf_token, auth_token, session)
+# api.createChart_experimentInformation(url, csrf_token, auth_token, session)
+# api.createChart_questions(url, csrf_token, auth_token, session)
+# api.createChart_numberOfWatches(url, csrf_token, auth_token, session)
+# api.createChart_hr(url, csrf_token, auth_token, session)
 
-api.createStandarizedDashboard(url, csrf_token, auth_token, session)
 
-#api.getDatasets(url, auth_token)
-#api.getCharts(url, auth_token)
-#api.getDashboards(url, auth_token)
-#api.get_charts_from_dashboard(api.getCharts(url, auth_token), 3)
-
-#api.createChart_numberOfSessions(url, csrf_token, auth_token, session)
-#api.createChart_experimentInformation(url, csrf_token, auth_token, session)
-#api.createChart_questions(url, csrf_token, auth_token, session)
-#api.createChart_numberOfWatches(url, csrf_token, auth_token, session)
-#api.createChart_hr(url, csrf_token, auth_token, session)
-
-#api.updateDashboard(url, csrf_token, auth_token, session)
+# api.updateDashboard(url, csrf_token, auth_token, session)
 
 session.close()
